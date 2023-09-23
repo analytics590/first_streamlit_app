be3c4646d044e092fc0c869a85d6f1b6dc6fc241
analytics590 <73377978+analytics590@users.noreply.github.com>
2023-09-23 19:49:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title("Hello WWorld")
+streamlit.title("My Parent new Healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled free-range Egg")
+
